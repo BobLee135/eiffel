eca7bc8d8c725263cf0ad96563e648d052d89168
boblee <jacques.artale@hotmail.com>
2023-04-01 19:56:02 +0200

    fix: bridge can now connect to rabbitmq

--- local-bridge/Rabbit/src/Rabbit/Rabbit.java
+++ local-bridge/Rabbit/src/Rabbit/Rabbit.java
@@ -19,7 +19,8 @@ public class Rabbit {
         MongoDatabase database = mongoClient.getDatabase("meteor");
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost("13.49.183.142");
+        factory.setPort(5672);
         factory.setUsername("rabbitmquser");
         factory.setPassword("rabbitmqpassword");
 
