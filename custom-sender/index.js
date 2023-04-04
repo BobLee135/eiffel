26206befe59bb951b2b8d79958c4ebfcc3f2ee6b
boblee <jacques.artale@hotmail.com>
2023-04-04 16:05:51 +0200

    fix: added new ip address of instances

--- custom-sender/index.js
+++ custom-sender/index.js
@@ -16,7 +16,7 @@ let config = {
 };
 
 async function login() {
-  await Axios.post("http://13.50.194.101:9000/login", {
+  await Axios.post("http://16.170.107.18:9000/login", {
     name: "Albin",
     password: "password123"
   }).then(response => {
@@ -100,7 +100,7 @@ async function artifactCreatedEvent() {
   };
 
   await Axios.post(
-    "http://13.50.194.101:9000/submitevent",
+    "http://16.170.107.18:9000/submitevent",
     { eiffelDataObj, parameterObj },
     config
   ).then(function(response) {
